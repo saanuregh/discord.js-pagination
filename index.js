328f248dab14e81947bb3e099c19d002fd7e3df2
saanuregh <saanu09xs@gmail.com>
2019-06-30 23:00:53 +0530

    readme and cleaned up code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout = 120000) => {
-	if (!msg.channel) throw new Error('Channel is inaccessible.');
+	if (!msg && !msg.channel) throw new Error('Channel is inaccessible.');
+	if (!pages) throw new Error('Pages are not given.');
+	if (emojiList.length !== 2) throw new Error('Need two emojis.');
 	let page = 0;
 	const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
 	for (const emoji of emojiList) await curPage.react(emoji);
