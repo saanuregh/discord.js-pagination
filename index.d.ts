cc739654df7ce9b40d8b22ed1c7020c1afc19099
unknown <psionatix@gmail.com>
2021-01-18 19:13:05 +1030

    Add custom footer and page resolvers, shift all optional and default arguments into an options object.

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import { Message, MessageEmbed, MessageReaction, ReactionCollectorOptions, User } from "discord.js";
+
+interface PaginationOptions extends ReactionCollectorOptions {
+  deleteOnEnd?: boolean,
+  useUtil?: boolean,
+  emojiList?: string[],
+  collectorFilter(reaction: MessageReaction, user: User): boolean | Promise<boolean>
+  footerResolver(pageIndex: number, pagesLength: number): string,
+  pageResolver(pages: MessageEmbed[], emojiList: string[], currentPageIndex: number, reaction: MessageReaction): number | Promise<number>
+}
+
+declare function paginationEmbed (msg: Message, pages: MessageEmbed[], paginationOptions?: PaginationOptions): Promise<Message>
+
+export = paginationEmbed;
--- index.js
+++ index.js
@@ -1,33 +1,75 @@
-const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout = 120000) => {
+/**
+ *
+ * @param {MessageEmbed[]} pages
+ * @param {string[]} emojiList
+ * @param {number} currentPageIndex
+ * @param {MessageReaction} reaction
+ * @returns {number} - the new page index.
+ */
+const defaultPageResolver = async (pages, emojiList, currentPageIndex, reaction) => {
+	let newPage = currentPageIndex;
+	switch (reaction.emoji.name) {
+		case emojiList[0]:
+			newPage = currentPageIndex > 0 ? currentPageIndex - 1 : pages.length - 1;
+			break;
+		case emojiList[1]:
+			newPage = currentPageIndex + 1 < pages.length ? currentPageIndex + 1 : 0;
+			break;
+		default:
+			return currentPageIndex;
+	}
+	return newPage;
+};
+
+const defaultFooterResolver = (currentPageIndex, pagesLength) =>`Page ${currentPageIndex + 1} / ${pagesLength}`;
+
+const defaultCollectorFilter = (reaction, user, emojiList) => emojiList.includes(reaction.emoji.name) && !user.bot;
+
+/**
+ *
+ * @param {Message} msg - the message
+ * @param {MessageEmbed[]} pages - array of message embeds to use as each page.
+ * @param {PaginationOptions} paginationOptions - exposes collector options, provides customization.
+ * @param {Function} footerResolver - Takes the current page index and number of pages, returns a string to be used for the footer.
+ */
+const paginationEmbed = async (
+										msg, pages,
+										{
+											deleteOnEnd = false, useUtil = false,
+											emojiList = ['⏪', '⏩'],
+											collectorFilter = defaultCollectorFilter,
+											footerResolver = defaultFooterResolver,
+											pageResolver = defaultPageResolver,
+											timeout = 120000,
+											...rest
+										} = {}) => {
 	if (!msg && !msg.channel) throw new Error('Channel is inaccessible.');
 	if (!pages) throw new Error('Pages are not given.');
 	if (emojiList.length !== 2) throw new Error('Need two emojis.');
 	let page = 0;
-	const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+	pages[page].setFooter(footerResolver(page, pages.length));
+	const curPage = useUtil ? await msg.util.reply(pages[page]) : await msg.channel.send(pages[page]);
 	for (const emoji of emojiList) await curPage.react(emoji);
 	const reactionCollector = curPage.createReactionCollector(
-		(reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
-		{ time: timeout }
+		(reaction, user) => collectorFilter(reaction, user, emojiList),
+		{ time: timeout, ...rest }
 	);
-	reactionCollector.on('collect', reaction => {
+	reactionCollector.on('collect', async reaction => {
 		reaction.users.remove(msg.author);
-		switch (reaction.emoji.name) {
-			case emojiList[0]:
-				page = page > 0 ? page -= 1 : pages.length - 1;
-				break;
-			case emojiList[1]:
-				page = page + 1 < pages.length ? page += 1 : 0;
-				break;
-			default:
-				break;
-		}
-		curPage.edit(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+		page = await pageResolver(pages, emojiList, page, reaction);
+
+		if (page >= 0 && page < pages.length && !curPage.deleted)
+			curPage.edit(pages[page].setFooter(footerResolver(page, pages.length)));
 	});
-	reactionCollector.on('end', () => {
+	reactionCollector.on('end', async () => {
 		if (!curPage.deleted) {
-			curPage.reactions.removeAll()
+			if (deleteOnEnd)
+				await curPage.delete();
+			else
+				curPage.reactions.removeAll();
 		}
 	});
 	return curPage;
 };
+
 module.exports = paginationEmbed;
