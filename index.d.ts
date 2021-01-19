9350fcaa01f55e332884d7f5814edafd2369d958
unknown <psionatix@gmail.com>
2021-01-19 18:17:44 +1030

    Typing syntax ',' to ';', add sendMessage property of PaginationOptions.

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
 import { EmojiIdentifierResolvable, Message, MessageEmbed, MessageReaction, ReactionCollectorOptions, User } from "discord.js";
 
 interface PaginationOptions extends ReactionCollectorOptions {
-  deleteOnEnd?: boolean,
-  useUtil?: boolean,
-  emojiList?: EmojiIdentifierResolvable[],
-  collectorFilter?(reaction: MessageReaction, user: User): boolean | Promise<boolean>
-  footerResolver?(pageIndex: number, pagesLength: number): string,
-  pageResolver?(msg: Message, pages: MessageEmbed[], emojiList: string[], currentPageIndex: number, reaction: MessageReaction): number | Promise<number>
+  deleteOnEnd?: boolean;
+  useUtil?: boolean;
+  emojiList?: EmojiIdentifierResolvable[];
+  collectorFilter?(reaction: MessageReaction, user: User): boolean | Promise<boolean>;
+  footerResolver?(pageIndex: number, pagesLength: number): string;
+  pageResolver?(msg: Message, pages: MessageEmbed[], emojiList: string[], currentPageIndex: number, reaction: MessageReaction): number | Promise<number>;
+  sendMessage?(message: Message, pageEmbed: MessageEmbed): Promise<Message>;
 }
 
-declare function paginationEmbed (msg: Message, pages: MessageEmbed[], paginationOptions?: PaginationOptions): Promise<Message>
+declare function paginationEmbed(msg: Message, pages: MessageEmbed[], paginationOptions?: PaginationOptions): Promise<Message>;
 
 export = paginationEmbed;
