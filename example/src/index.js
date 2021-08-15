ea402a0b35601dfafcff5b3eeba86d011ab9041b
psibean <psionatix@gmail.com>
2021-08-15 15:41:58 +0930

    Update example app to match latest refactor

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,11 @@
 const { Client, Intents, MessageEmbed } = require('discord.js');
-const { ReactionPaginationEmbed } = require('../../src/index');
+const { PaginationEvents, ReactionPaginationEmbed } = require('../../src/index');
+
+
+process.on('uncaughtException', function(err) {
+	console.log('UNCAUGHT EXCEPTION:\n');
+	console.log(err);
+});
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
@@ -10,7 +16,6 @@ client.once('ready', () => {
 client.on('messageCreate', async (message) => {
 	if (message.content.startsWith('!testreactions')) {
 		const myPages = [];
-
 		for (let i = 0; i < 10; i++) {
 			const pageEmbed = new MessageEmbed();
 			pageEmbed
@@ -18,28 +23,20 @@ client.on('messageCreate', async (message) => {
 				.setDescription(`That means it is page #${i + 1}`);
 			myPages.push(pageEmbed);
 		}
-		const reactionPaginationEmbed = new ReactionPaginationEmbed(message, myPages, {collectErrorHandler: ({ error }) => console.log('Error on collect\n' + error) });
+		const reactionPaginationEmbed = new ReactionPaginationEmbed(message, myPages)
+			.on(PaginationEvents.PAGINATION_END, async ({ reason, paginator }) => {
+				try {
+					console.clog('The pagination has ended: ' + reason);
+					if (!paginator.message.deleted)
+						await paginator.message.delete();
+				} catch (error) {
+					console.log('There was an error when deleting the message: ');
+					console.log(error);
+				}
+			})
+			.on(PaginationEvents.COLLECT_ERROR, ({ error }) => console.log(error));
 		const sentMessage = await reactionPaginationEmbed.send();
 		return sentMessage;
-	}
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-	if (interaction.isMessageComponent) console.log('THis was a message component interaction.');
-
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} if (interaction.commandName === 'testreactions') {
-		// const myPages = [];
-
-		// for (let i = 0; i < 10; i++) {
-		// 	const pageEmbed = new MessageEmbed();
-		// 	pageEmbed
-		// 		.setTitle(`This embed is index ${i}!`)
-		// 		.setDescription(`That means it is page #${i + 1}`);
-		// 	myPages.push(pageEmbed);
-		// }
 	}
 });
 
