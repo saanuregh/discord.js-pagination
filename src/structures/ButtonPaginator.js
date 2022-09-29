c0a5cc16e4ee5a20650305ebad3d935e9aacbd95
psibean <psionatix@gmail.com>
2022-09-30 08:32:57 +0930

    WIP update ButtonPaginator to use builders

--- src/structures/ButtonPaginator.js
+++ src/structures/ButtonPaginator.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { mergeDefault } = require('discord.js');
+const { mergeDefault, ButtonBuilder } = require('discord.js');
 const { ComponentType } = require('discord-api-types/v10');
 const ActionRowPaginator = require('./ActionRowPaginator');
 const ButtonPaginatorOptions = require('../util/ButtonPaginatorOptions');
@@ -22,9 +22,9 @@ class ButtonPaginator extends ActionRowPaginator {
         // LINK : PRIMARY - MessageButtonStyles doesn't work.
         if (!button.style) button.style = isLink ? 5 : 1;
         if (button.row > 0 && button.row < buttonRows.length) {
-          buttonRows[button.row].push(button);
+          buttonRows[button.row].push(ButtonBuilder.from(button));
         } else {
-          buttonRows[0].push(button);
+          buttonRows[0].push(ButtonBuilder.from(button));
         }
       }
       buttonRows.forEach((row, index) => {
